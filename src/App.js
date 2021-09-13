22d34f012c88de5124ff96c4979b689b577e2532
saurabh-dev15 <saurabh.singhdev15@gmail.com>
2021-09-13 21:02:28 +0600

    update 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
 import AuthButton from 'relysia-sdk';
+import { useState } from 'react';
 
 function App() {
+  const {packageResp, setPackageResp} = useState('Token');
   return (
     <div className="App">
+      <h1>Using React npm package</h1>
       <AuthButton
         clientKey="7c9a952e-1bd1-4ac5-a0cc-ecfaeb9d09ed"
         clientSecret="NCMCWFHbNcdbcIASPCbACdAHFJAPIYNLaVTNYBJEEGRKaQGTIYQTUXNLeQATLG"
         // isSignedIn={loggedIn}
         onSuccess={(resp) => {
           console.log(resp);
+          setPackageResp(resp);
         }}
         onError={(errorResp) => {
           console.log(errorResp);
         }}
         customComponent={<div>Relysia OAuth Login</div>}
       />
+      <h1>Response : {packageResp}</h1>
     </div>
   );
 }
