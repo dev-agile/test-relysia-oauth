cdce9603b41f5059bc87e1fe439178cf19efc67c
saurabh-dev15 <saurabh.singhdev15@gmail.com>
2021-09-13 21:16:25 +0600

    update 3

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import AuthButton from 'relysia-sdk';
 import { useState } from 'react';
 
 function App() {
-  const {packageResp, setPackageResp} = useState('Token');
+  const [packageResp, setPackageResp] = useState({});
   return (
     <div className="App">
-      <h1>Using React npm package</h1>
-      <AuthButton
-        clientKey="7c9a952e-1bd1-4ac5-a0cc-ecfaeb9d09ed"
-        clientSecret="NCMCWFHbNcdbcIASPCbACdAHFJAPIYNLaVTNYBJEEGRKaQGTIYQTUXNLeQATLG"
-        // isSignedIn={loggedIn}
-        onSuccess={(resp) => {
-          console.log(resp);
-          setPackageResp(resp);
-        }}
-        onError={(errorResp) => {
-          console.log(errorResp);
-        }}
-        customComponent={<div>Relysia OAuth Login</div>}
-      />
-      <h1>Response : {packageResp}</h1>
+      <div>
+        <h1>Using React npm package</h1>
+        <AuthButton
+          clientKey="7c9a952e-1bd1-4ac5-a0cc-ecfaeb9d09ed"
+          clientSecret="NCMCWFHbNcdbcIASPCbACdAHFJAPIYNLaVTNYBJEEGRKaQGTIYQTUXNLeQATLG"
+          // isSignedIn={loggedIn}
+          onSuccess={(resp) => {
+            console.log(resp);
+            setPackageResp(resp);
+          }}
+          onError={(errorResp) => {
+            console.log(errorResp);
+          }}
+          customComponent={<div>Relysia OAuth Login</div>}
+        />
+        {packageResp.data && <h1>Response From Npm package :</h1>}
+        {packageResp.data && packageResp.data.status && <h3>Status : {packageResp.data.status}</h3>}
+        {packageResp.data && packageResp.data.msg && <h3>Token : {packageResp.data.msg}</h3>}
+      </div>
     </div>
   );
 }
